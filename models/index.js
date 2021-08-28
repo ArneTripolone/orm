17ceedf53c9aaff291f3d1018031989eb6607a67
Arne Tripolone <arnetripolone@gmail.com>
2021-08-28 10:27:32 +1000

    Models created. Issue with models index - needs debugging

--- models/index.js
+++ models/index.js
@@ -5,16 +5,77 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'dunno1'
+});
 
 // Categories have many Products
+Category.belongsTo(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'dunno1'
+});
 
 // Products belongToMany Tags (through ProductTag)
+// Products belongsTo Category
+Product.hasMany(Tag, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: Tag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'dunno2'
+});
+
 
 // Tags belongToMany Products (through ProductTag)
+// Products belongsTo Category
+Tag.hasMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: Tag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'dunno2'
+});
 
 module.exports = {
   Product,
   Category,
   Tag,
-  ProductTag,
+  ProductTag
 };
+
+/*
+// Products belongsTo Category
+Product.belongsToMany(Category, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: product,
+  },
+  // Define an alias for when data is retrieved
+  as: 'dunno'
+});
+
+// Categories have many Products
+Category.belongsToMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: product,
+  },
+  // Define an alias for when data is retrieved
+  as: 'dunno_again'
+});
+*/
